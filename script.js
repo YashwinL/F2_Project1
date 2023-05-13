1c66e5527b4022a5f51bc82e456863dcf59ba14c
YashwinL <yashwinlakshmipathy@gmail.com>
2023-05-13 13:27:36 +0530

    Solution

--- script.js
+++ script.js
@@ -8,20 +8,47 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  let result = arr.map((item) => {
+    if (item.marks > 50) {
+      return item;
+    }
+  });
+  console.log(result);
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((item) => {
+    if (item.marks > 50) {
+      console.log(item);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({ id: 4, name: "susan", age: "20", marks: 45 });
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  let result = arr.filter((item) => item.marks < 50);
+  console.log(result);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+
+  let Newarr = [];
+  arr.forEach((items) =>
+    Newarr.push({
+      id: items.id + 1,
+      name: items.name,
+      age: items.age,
+      marks: items.marks,
+    })
+  );
+
+  console.log(Newarr);
 }
